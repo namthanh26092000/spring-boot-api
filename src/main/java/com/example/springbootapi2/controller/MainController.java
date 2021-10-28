bec1c3b9e521e4d2155535f5cb7dc1341d6a473f
Nam Bui <buithanhnam26092000@gmail.com>
2021-10-28 08:40:33 +0700

    aa

--- src/main/java/com/example/springbootapi2/controller/MainController.java
+++ src/main/java/com/example/springbootapi2/controller/MainController.java
@@ -1,4 +1,5 @@
 package com.example.springbootapi2.controller;
+
 import com.example.springbootapi2.entity.Students;
 import com.example.springbootapi2.repository.StudentRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class MainController {
@@ -15,27 +17,38 @@ public class MainController {
     private StudentService studentService;
 
     @GetMapping("/students")
-    public List<Students> getAll(){
+    public List<Students> getAll() {
         return studentService.getAllStudents();
     }
 
     @GetMapping("/students/{id}")
-    public Students findStudentById(@PathVariable("id") Long id){
+    public Students findStudentById(@PathVariable("id") Long id) {
         return studentService.getStudentById(id);
     }
 
     @PostMapping("/students")
-    public void addStudent(@RequestBody Students student){
+    public void addStudent(@RequestBody Students student) {
         studentService.addStudent(student);
     }
 
     @PostMapping("/students/{id}")
-    public void delStudent(@PathVariable("id") Long id){
+    public void delStudent(@PathVariable("id") Long id) {
         studentService.delStudent(id);
     }
 
     @PutMapping("/students/{id}")
-    public Students updateStudent(@PathVariable("id") Long id, @RequestBody Students student){
+    public Students updateStudent(@PathVariable("id") Long id, @RequestBody Students student) {
         return studentService.updateStudent(id, student);
     }
+
+    // @PutMapping("/students/{id}")
+    // public Students updateStudent(@PathVariable("id") Long id, @RequestBody
+    // Students student){
+    // return studentService.updateStudent(id, student);
+    // }
+    @PutMapping("/students/CQQ{id}")
+    public Students update(@PathVariable("id") Long id, @RequestBody Students student) {
+        return studentService.updateStudent(id, student);
+    }
+
 }
